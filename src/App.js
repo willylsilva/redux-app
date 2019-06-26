c54ba55f74c5a334ee9a478050b63023d8c37983
Danilo Vitoriano <dvitoriano@uolinc.com>
2019-06-26 16:30:14 -0300

    clean redux app

--- src/App.js
+++ src/App.js
@@ -1,39 +1,18 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { clickButton } from './actions';
 import './App.css';
 
 class App extends Component {
-  state = {
-    inputValue: ''
-  }
-
-  inputChange = event => {
-    this.setState({
-      inputValue: event.target.value
-    })
-  }
 
   render() {
-    const { newValue, clickButton } = this.props;
-    const { inputValue } = this.state;
     return (
       <div className="App" style={{ paddingTop: '10px' }}>
-        <input onChange={this.inputChange} type='text' value={inputValue} />
-        <button onClick={() => clickButton(inputValue)}>
+        <input type='text' />
+        <button>
           Click me!
         </button>
-        <h1>{newValue}</h1>
+        <h1>texto</h1>
       </div>
     );
   }
 }
-const mapStateToProps = store => ({
-  newValue: store.clickState.newValue
-});
-
-const mapDispatchToProps = dispatch =>
-  bindActionCreators({ clickButton }, dispatch);
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
--- src/reducers/clickReducer.js
+++ src/reducers/clickReducer.js
@@ -1,16 +1,3 @@
-import { CLICK_UPDATE_VALUE } from '../actions/actionTypes';
-
-const initialState = {
+export const initialState = {
     newValue: 'Atualizado via Redux!'
-  };
-  export const clickReducer = (state = initialState, action) => {
-    switch (action.type) {
-      case CLICK_UPDATE_VALUE:
-        return {
-          ...state,
-          newValue: action.newValue
-        };
-      default:
-        return state;
-    }
   };
